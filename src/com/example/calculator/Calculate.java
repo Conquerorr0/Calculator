da0607b2c77aba47db46bbd78b3f2e8e4d5af12e
esraoguz1903 <100204591+esraoguz1903@users.noreply.github.com>
2024-02-17 17:13:06 +0300

    işlemleri içeren metotlar eklendi

---
+++ src/com/example/calculator/Calculate.java
@@ -0,0 +1,21 @@
+package com.example.calculator;
+class Calculate{
+   public int topla(int a, int b){
+       return a+b;
+   }
+   
+   public int cikar(int a, int b){
+       return a-b;
+   }
+   
+   public int carp(int a, int b){
+       return a*b;
+   }
+   
+   public int bol(int a, int b){
+       return a/b;
+   }
+   
+        
+    
+}
