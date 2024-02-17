ab8088658ec9a5f112f6a6bb91aaa2c8207379f9
esraoguz1903 <100204591+esraoguz1903@users.noreply.github.com>
2024-02-17 22:40:17 +0300

    işlemler düzenlendi try catch eklendi.

--- src/com/example/calculator/Calculate.java
+++ src/com/example/calculator/Calculate.java
@@ -1,21 +1,37 @@
 package com.example.calculator;
 class Calculate{
-   public int topla(int a, int b){
-       return a+b;
-   }
+    public int hesapla(){
+        double sayi = 0,sonuc =0;
+        char islem = 0;
+        switch (islem) {
+            case '+':
+                sonuc += sayi;    
+                break;
+            case '-':
+                sonuc -= sayi;
+                break;
+            case '*':
+                sonuc *= sayi;
+                break;
+            case '/':
+                try{
+                    sonuc /= sayi;
+                }
+                catch(ArithmeticException e){
+                    System.out.println("0 'a bölme hatası");
+                }
+                break;
+            case '%':
+                sonuc = sonuc * sayi / 100;
+                break;
+                   
+            default:
+                throw new AssertionError();
+        }
+        return 0;
+    }
+}
    
-   public int cikar(int a, int b){
-       return a-b;
-   }
-   
-   public int carp(int a, int b){
-       return a*b;
-   }
-   
-   public int bol(int a, int b){
-       return a/b;
-   }
-   
+  
         
     
-}
